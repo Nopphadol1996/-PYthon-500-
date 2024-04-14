2db9db9076ddf7231525e5992ca931d93c8812bc
Nopphadol Kanklaew <89741372+Nopphadol1996@users.noreply.github.com>
2024-04-14 08:46:53 +0700

    Add files via upload

---
+++ 4.Data Strcture/String.py
@@ -0,0 +1,151 @@
+### 19 ###
+print('ข้อ19')
+string1 = 'Python'
+print(string1)
+print(type(string1))
+print('------------------------------\n')
+
+### 20 ###
+print('ข้อ20')
+string1 = 'Python'
+print(string1[0])
+print('------------------------------\n')
+
+
+### 21 ###
+print('ข้อ21')
+string1 = 'Python'
+print(string1[1])
+print('------------------------------\n')
+
+### 22 ###
+print('ข้อ22')
+string1 = 'Python'
+print(string1[-1])
+print('------------------------------\n')
+
+### 23 ###
+print('ข้อ23')
+string1 = 'Python'
+print(string1[-2])
+print('------------------------------\n')
+
+### 24 ###
+print('ข้อ24')
+string1 = 'Python'
+print(string1[2])
+print(string1[-4])
+print('------------------------------\n')
+
+### 25 ###
+print('ข้อ25')
+string1 = 'Python'
+print(string1[3])
+print(string1[-3])
+print('------------------------------\n')
+
+
+### 26 ###
+print('ข้อ26')
+string1 = 'Python'
+print(string1[1:4])
+print('------------------------------\n')
+
+### 27 ###
+print('ข้อ27')
+string1 = 'Python'
+print(string1[2:-1])
+print('------------------------------\n')
+
+### 28 ###
+print('ข้อ28')
+string1 = 'Python'
+print(string1[2:])
+print('------------------------------\n')
+
+### 29 ###
+print('ข้อ29')
+string1 = 'Python'
+print(string1[-4:5])
+print('------------------------------\n')
+
+### 30 ###
+print('ข้อ30')
+string1 = 'Python'
+print(string1[-6:5])
+print(string1[:5])
+print('------------------------------\n')
+
+
+### 31 ###
+print('ข้อ31')
+string1 = 'Python'
+print(string1[:4])
+print('------------------------------\n')
+
+
+### 32 ###
+# print('ข้อ32')
+# string = input('Please insert the  letter: ')
+# print(len(string))
+print('------------------------------\n')
+
+
+### 33 ###
+print('ข้อ33')
+# string = input('Please insert letter: ')
+# print('ก' in string)
+print('------------------------------\n')
+
+### 34 ###
+print('ข้อ34')
+# string1 = input('Please insert letter1: ')
+# string2 = input('Please insert letter2: ')
+
+# print(string2 in string1)
+
+print('------------------------------\n')
+
+
+### 35 ###
+print('ข้อ35')
+# string1 = input('Please insert letter: ')
+# print(string1.replace('a','A'))
+
+print('------------------------------\n')
+
+### 36 ###
+print('ข้อ36')
+# string1 = input('Please insert letter: ')
+# string2 = string1.replace('a','A')
+# print(string2)
+
+print('------------------------------\n')
+
+### 37 ###
+print('ข้อ37')
+# sentence = input('Please insert letter: ')
+# print(sentence.split(' '))
+
+print('------------------------------\n')
+
+### 38 ###
+print('ข้อ38')
+# sentence = input('Please insert letter: ')
+# c = input('pls take your letter')
+
+# print(sentence.split('{}'.format(c)))
+# print(sentence.split(c))
+
+print('------------------------------\n')
+
+
+### 39 ###
+print('ข้อ39')
+str1 = input('Please insert str1: ')
+str2 = input('Please insert str2: ')
+
+print(str1+str2)
+
+
+print('------------------------------\n')
